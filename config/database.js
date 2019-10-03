91edfaea9b7d579b8f72a1bbe24b2638c82c4de7
ManliestBen <benjamintmanley@gmail.com>
2019-10-03 18:52:38 -0500

    fix mongoDB link

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 
 
-mongoose.connect(process.env.REACT_APP_DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 // database connection:
 mongoose.connection.on('connected', function () {
     console.log(`Mongoose connected to database online`);
