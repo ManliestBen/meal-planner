326af1bb98c161c91bc17ac88a365c01aad86ec9
ManliestBen <benjamintmanley@gmail.com>
2019-10-09 15:58:58 -0500

    fix deployment keys

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb+srv://sei:${process.env.DATABASE_PW}@sei-1hogc.gcp.mongodb.net/meal-planner?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 // database connection:
 mongoose.connection.on('connected', function () {
     console.log(`Mongoose connected to database online`);
